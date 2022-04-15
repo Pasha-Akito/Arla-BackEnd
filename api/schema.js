7cb2493c18589a6446326dc110658316a4888a5b
Pasha-Akito <1masterpasha@gmail.com>
2022-04-15 09:27:54 +0100

    added Post type and relationships

--- api/schema.js
+++ api/schema.js
@@ -23,7 +23,8 @@ type User {
   tokenId: String! @unique
   permissions: [String]
   email: String
-  profile: Person @relationship(type: "IS_PERSON", direction: OUT)
+  profile: Person! @relationship(type: "IS_PERSON", direction: OUT)
+  friends: [Person!]! @relationship(type: "FRIENDS_WITH", direction: OUT)
 }
 
 type Person {
@@ -32,9 +33,11 @@ type Person {
   image: String
   id: ID! @id
   country: String
-  user: User @relationship(type: "IS_PERSON", direction: IN)
+  user: User! @relationship(type: "IS_PERSON", direction: IN)
   graduatedCourses: [Course!]! @relationship(type: "GRADUATED", direction: OUT, properties: "Graduated")
   interests: [Interest!]! @relationship(type: "INTEREST_IN", direction: OUT)
+  posts: [Post!]! @relationship(type: "POSTED_BY", direction: OUT, properties: "PostedAt")
+  friends: [User!]! @relationship(type: "FRIENDS_WITH", direction: IN)
 }
 
 type Interest {
@@ -51,13 +54,19 @@ type Course {
   peopleGraduated: [Person!]! @relationship(type: "GRADUATED", direction: IN, properties: "Graduated")
 }
 
+type Post {
+  id: ID! @id
+  content: String!
+  creator: Person! @relationship(type: "POSTED_BY", direction: IN, properties: "PostedAt")
+  createdAt: DateTime!
+}
+
 interface Graduated {
   year: Int
 }
 
-enum Role {
-  USER
-  ADMIN
+interface PostedAt {
+    date: DateTime
 }
 
 `;
